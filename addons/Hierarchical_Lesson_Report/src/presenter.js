f6a43b5dd455bb9b61b90891e312ec62c95bf0c8
hadhod <maciej.mlynarski@solwit.com>
2014-09-08 10:23:33 +0200

    pierwsze zmiany

--- addons/Hierarchical_Lesson_Report/src/presenter.js
+++ addons/Hierarchical_Lesson_Report/src/presenter.js
@@ -5,7 +5,6 @@ function AddonHierarchical_Lesson_Report_create() {
     var totalChecks = 0;
     var totalErrors = 0;
     var totalMistakes = 0;
-    var chapterScore = null;
     var isPreview = false;
 
     var pagesMockup = [
@@ -26,8 +25,15 @@ function AddonHierarchical_Lesson_Report_create() {
     ];
 
     presenter.ERROR_MESSAGES = {
-        EXPAND_DEPTH_NOT_NUMERIC: "Depth of expand is not proper"
+        EXPAND_DEPTH_NOT_NUMERIC: "Depth of expand is not proper",
+
+        C01: "Wrong classes name format",
+        C02: "Class names has to be in every new line" // TODO poprawic ten tekst
     };
+
+    function returnErrorObject(ec) { return { isValid: false, errorCode: ec }; }
+
+    function returnCorrectObject(v) { return { isValid: true, value: v }; }
 
     presenter.showErrorMessage = function (message, substitutions) {
         var errorContainer;
@@ -116,20 +122,17 @@ function AddonHierarchical_Lesson_Report_create() {
     }
 
     function createRow(index, parrentIndex, isChapter) {
-        row = document.createElement('tr');
-        $(row).appendTo($("#" + presenter.treeID).find('table'))
-            .addClass("treegrid-" + index);
+        var row = document.createElement('tr');
+
+        $(row).appendTo($("#" + presenter.treeID).find('table')).addClass("treegrid-" + index);
+
         if (parrentIndex != null) {
         	$(row).addClass("treegrid-parent-" + parrentIndex);
         }
         if (isChapter) {
         	$(row).addClass("hier_report-chapter");
         } else {
-            if (index % 2 > 0) {
-            	$(row).addClass("hier_report-odd");
-            } else {
-            	$(row).addClass("hier_report-even");
-            }
+            $(row).addClass(index % 2 > 0 ? "hier_report-odd" : "hier_report-even");
         }
         return row;
     }
@@ -140,7 +143,7 @@ function AddonHierarchical_Lesson_Report_create() {
 
         progressbar = document.createElement('div');
         $(progressbar).appendTo($(progressCell));
-        $(progressbar).attr("id", "progressbar-" + index)
+        $(progressbar).attr("id", "progressbar-" + index);
         $(progressbar).addClass("hier_report-progressbar");
 
         var percent = Math.floor(score.score / score.count * 100);
@@ -202,15 +205,9 @@ function AddonHierarchical_Lesson_Report_create() {
                 .text(text)
                 .attr('href', '#')
                 .attr('data-page-id', pageId);
-
         
         var wrapper = $('<div class="text-wrapper">');
-        if (isChapter) {
-        	wrapper.html(text);
-        } else {
-        	wrapper.html($link);
-        }
-
+        wrapper.html(isChapter ? text : $link);
         $element.append(wrapper);
         return $element;
     }
@@ -241,15 +238,15 @@ function AddonHierarchical_Lesson_Report_create() {
         }
 
         if (presenter.configuration.showChecks) {
-            $(row).find(".hier_report-checks").html((isEmptyChapter)?"-": pageScore.checkCount);
+            $(row).find(".hier_report-checks").html(isEmptyChapter ? "-" : pageScore.checkCount);
         }
 
         if (presenter.configuration.showMistakes) {
-            $(row).find(".hier_report-mistakes").html((isEmptyChapter)?"-": pageScore.mistakeCount);
+            $(row).find(".hier_report-mistakes").html(isEmptyChapter ? "-" : pageScore.mistakeCount);
         }
 
         if (presenter.configuration.showErrors) {
-            $(row).find(".hier_report-errors").html((isEmptyChapter)?"-": pageScore.errorCount);
+            $(row).find(".hier_report-errors").html(isEmptyChapter ? "-" : pageScore.errorCount);
         }
     }
 
@@ -263,15 +260,14 @@ function AddonHierarchical_Lesson_Report_create() {
     }
 
     function resetScore() {
-        var score = {
+        return {
             score: 0,
             maxScore: 0,
             errorCount: 0,
             checkCount: 0,
             mistakeCount: 0,
             count: 0
-        }
-        return score;
+        };
     }
 
     presenter.createPreviewTree = function () {
@@ -280,7 +276,7 @@ function AddonHierarchical_Lesson_Report_create() {
             addRow(pagesMockup[i].name, i, pagesMockup[i].parrent, false, pagesMockup[i].pageId);
         }
         return chapterScore;
-    }
+    };
 
     presenter.createTree = function (root, parrentIndex, pageCount) {
         var chapterIndex = 0,
@@ -291,7 +287,7 @@ function AddonHierarchical_Lesson_Report_create() {
 
         for (var i = 0; i < pageCount; i++) {
             var isChapter = (root.get(i).type == "chapter");
-            if (!isChapter && !root.get(i).isReportable())  continue;
+            if (!isChapter && !root.get(i).isReportable()) continue;
             if (!isChapter && root.get(i).isReportable()) {
             	isEmpty = false;
             }
@@ -312,7 +308,7 @@ function AddonHierarchical_Lesson_Report_create() {
            	chapterScore = updateScore(chapterScore, pageScore);
         }
         return {pagesScore: chapterScore, isEmpty:isEmpty};
-    }
+    };
 
     function handleMouseClickActions() {
         var commander = presentationController.getCommands(),
@@ -352,20 +348,15 @@ function AddonHierarchical_Lesson_Report_create() {
 
     function restoreTreeState(state) {
         $('.hier_report table').find('tr').not('.hier_report-header').not('.hier_report-footer').each(function () {
-            if (state[$(this).treegrid('getNodeId')]) {
-                $(this).treegrid('expand');
-            } else {
-                $(this).treegrid('collapse');
-            }
+            $(this).treegrid(state[$(this).treegrid('getNodeId')] ? 'expand' : 'collapse');
         });
     }
 
     presenter.getState = function () {
-        var state = {
+        return JSON.stringify({
             'treeState': saveTreeState(),
             'isVisible': presenter.configuration.isVisible
-        };
-        return JSON.stringify(state);
+        });
     };
 
     presenter.setState = function (stateString) {
@@ -377,15 +368,45 @@ function AddonHierarchical_Lesson_Report_create() {
         presenter.configuration.isVisible = state.isVisible;
     };
 
+    function parseClasses(classes_text) {
+        function isValidClassName(class_name) {
+            return /^[a-z_-][a-z\d_-]*$/i.test(class_name);
+        }
+
+        if (classes_text === "") {
+            return returnCorrectObject(null);
+        }
+
+        var classes = classes_text.split('\n');
+
+        for (var i=0; i<classes.length; i++) {
+            classes[i] = classes[i].trim();
+            console.log(classes[i]);
+            if (classes[i].indexOf(' ') !== -1) {
+                return returnErrorObject("C02");
+            }
+
+            if (isValidClassName(classes[i])) {
+                return returnErrorObject("C01");
+            }
+        }
+
+        return returnCorrectObject(classes);
+    }
 
     presenter.validateModel = function (model) {
         var expandDepth = {value:0, isValid: true};
 
         if (model['expandDepth'].length > 0) {
-            var expandDepth = ModelValidationUtils.validateInteger(model['expandDepth']);
+            expandDepth = ModelValidationUtils.validateInteger(model['expandDepth']);
             if (!expandDepth.isValid) {
-                return { isValid: false, errorCode: 'EXPAND_DEPTH_NOT_NUMERIC' };
+                return returnErrorObject('EXPAND_DEPTH_NOT_NUMERIC');
             }
+        }
+
+        var validatedClasses = parseClasses(model["classes"]);
+        if (!validatedClasses.isValid) {
+            return returnErrorObject(validatedClasses.errorCode);
         }
 
         return {
@@ -423,11 +444,7 @@ function AddonHierarchical_Lesson_Report_create() {
         }
 
         $('.hier_report').attr("style", "height: " + presenter.configuration.height + "px");
-        if (isPreview) {
-            presenter.treeID = presenter.configuration.ID + "Preview";
-        } else {
-            presenter.treeID = presenter.configuration.ID;
-        }
+        presenter.treeID = presenter.configuration.ID + isPreview ? "Preview" : "";
         presenter.$view.find("div").first().attr('id', presenter.treeID);
 
         presenter.setVisibility(presenter.configuration.isVisible);
@@ -444,14 +461,14 @@ function AddonHierarchical_Lesson_Report_create() {
 
         $("#" + presenter.treeID).find('table').not('.hier_report-header').not('.hier_report-footer').treegrid({
             'initialState': 'collapsed',
-            'expanderTemplate': '<div class="treegrid-expander"></div>',
+            'expanderTemplate': '<div class="treegrid-expander"></div>'
         });
 
         expandTree(presenter.configuration.expandDepth);
         if (!isPreview) {
             handleMouseClickActions();
         }
-    }
+    };
 
     return presenter;
 }
